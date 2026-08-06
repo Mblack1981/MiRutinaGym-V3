efa44cc6cad1e434c9c35e7953c68c8f48ac018f
Alex <alex@air-de-alex.home>
2026-08-06 17:25:06 +0200

    Añadir imagenes al cache offline

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "MiRutinaGym-v3-estable";
+const CACHE_NAME = "MiRutinaGym-v3.1-offline";
 
 const FILES_TO_CACHE = [
 
@@ -22,10 +22,46 @@ const FILES_TO_CACHE = [
 
     "manifest.json",
 
-    "img/icon-192.png",
+       "img/icon-192.png",
     "img/icon-512.png",
     "img/apple-touch-icon.png",
-    "img/favicon-32.png"
+    "img/favicon-32.png",
+
+    "img/abductores.webp",
+    "img/aperturas_peckdeck.webp",
+    "img/crunch_polea.webp",
+    "img/curl_biceps.webp",
+    "img/curl_femoral.webp",
+    "img/curl_martillo.webp",
+    "img/curl_scott.webp",
+    "img/dominadas.webp",
+    "img/elevacion_gemelos.webp",
+    "img/elevaciones_laterales.webp",
+    "img/elevaciones_piernas.webp",
+    "img/encogimientos.webp",
+    "img/extension_triceps.webp",
+    "img/extension_triceps_mancuerna.webp",
+    "img/extensiones_cuadriceps.webp",
+    "img/face_pull.webp",
+    "img/fondos.webp",
+    "img/hip_thrust.webp",
+    "img/jalon_agarre_estrecho.webp",
+    "img/jalon_al_pecho.webp",
+    "img/peso_muerto_rumano.webp",
+    "img/plancha.webp",
+    "img/prensa.webp",
+    "img/prensa_unilateral.webp",
+    "img/press_banca.webp",
+    "img/press_frances.webp",
+    "img/press_inclinado.webp",
+    "img/press_militar_sentado.webp",
+    "img/pullover.webp",
+    "img/remo_con_barra.webp",
+    "img/remo_mancuerna.webp",
+    "img/remo_polea.webp",
+    "img/remo_t_bar.webp",
+    "img/rueda_abdominal.webp",
+    "img/sentadilla.webp"
 
 ];
 
